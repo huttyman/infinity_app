0bec58ffdaecbb746614f269f1e589710e6180ec
huttyman_456 <tossapong.p@gmail.com>
2020-09-12 04:22:11 +0700

    change to collapsable

--- App.js
+++ App.js
@@ -1,140 +1,342 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Color from './templates/Colors';
+/*Example of Collapsible - Accordion - Expandable View in React Native*/
+import React, { Component } from 'react';
+//import react in our project
+import {
+  Switch,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+//import basic react native components
+import * as Animatable from 'react-native-animatable';
+//import for the animation of Collapse and Expand
+import Collapsible from 'react-native-collapsible';
+//import for the collapsible/Expandable view
+import Accordion from 'react-native-collapsible/Accordion';
+//import for the Accordion view
+import Cart from './components/Cart'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+//Dummy content to show
+//You can also use dynamic data by calling webservice
+const CONTENT = [
+  {
+    title: 'Terms and Conditions',
+    content:
+      'The following terms and conditions, together with any referenced documents (collectively, "Terms of Use") form a legal agreement between you and your employer, employees, agents, contractors and any other entity on whose behalf you accept these terms (collectively, “you” and “your”), and ServiceNow, Inc. (“ServiceNow,” “we,” “us” and “our”).',
+  },
+  {
+    title: 'Privacy Policy',
+    content:
+      'A Privacy Policy agreement is the agreement where you specify if you collect personal data from your users, what kind of personal data you collect and what you do with that data.',
+  },
+  {
+    title: 'Return Policy',
+    content:
+      'Our Return & Refund Policy template lets you get started with a Return and Refund Policy agreement. This template is free to download and use.According to TrueShip study, over 60% of customers review a Return/Refund Policy before they make a purchasing decision.',
+  },
+];
+const CONTENT2 = [
+  {
+    title: 'Terms and Cons',
+    content:
+      'The following terms and conditions, together with any referenced documents (collectively, "Terms of Use") form a legal agreement between you and your employer, employees, agents, contractors and any other entity on whose behalf you accept these terms (collectively, “you” and “your”), and ServiceNow, Inc. (“ServiceNow,” “we,” “us” and “our”).',
+  },
+  {
+    title: 'Privfasdfadsfadsicy',
+    content:
+      'A Privacy adsfsadfsadt personal data from your users, what kind of personal data you collect and what you do with that data.',
+  },
+  {
+    title: 'Return Policy',
+    content:
+      'Our Return & Refund Policy template lets you get started with a Return and Refund Policy agreement. This template is free to download and use.According to TrueShip study, over 60% of customers review a Return/Refund Policy before they make a purchasing decision.',
+  },
+];
+const ARMY_CONTENT = [
+  {
+    title: 'Fusiliers',
+    attribute: {
+        mov: '4-4',
+        cc: '13',
+        bs: '12',
+        ph: '10',
+        wip: '12',
+        arm: '1',
+        bts: '0',
+        w: '1',
+        s: '2',
+        ava: 'T',
+    },
+    mainGunName: 'CombiRifle',
+    mainGunAttribute: {
+      dam: '13',
+      b:'3',
+      ammo: 'N',
+      save: 'arm',
+      range: {
+        short_range:'0-8"',
+        short_mod: '-3',
+        medium_range:'8-16"',
+        medium_mod: '+3',
+        long_range: '16-32"',
+        long_mod: '-3',
+        max_range: '32-48"',
+        max_attr: '-6'
+      }
+    },
+    
 
-      <View style={styles.listContainer}>
-        <View style={styles.troopTitle}>
-          <Text style={styles.troopTitleText}>Fusiliers</Text>
-        </View>
-        <View style={styles.detailContainer}>
-          <View style={styles.rowContainer}>
-            <View style={styles.rowWidth}><Text>MOV</Text></View>
-            <View style={styles.rowWidth}><Text>CC</Text></View>
-            <View style={styles.rowWidth}><Text>BS</Text></View>
-            <View style={styles.rowWidth}><Text>PH</Text></View>
-            <View style={styles.rowWidth}><Text>WIP</Text></View>
-            <View style={styles.rowWidth}><Text>ARM</Text></View>
-            <View style={styles.rowWidth}><Text>BTS</Text></View>
-            <View style={styles.rowWidth}><Text>W</Text></View>
-            <View style={styles.rowWidth}><Text>S</Text></View>
-            <View style={styles.rowWidth}><Text>AVA</Text></View>
+  },
+  {
+    title: 'Nokken',
+    attribute: {
+        mov: '4-4',
+        cc: '13',
+        bs: '12',
+        ph: '10',
+        wip: '13',
+        arm: '2',
+        bts: '3',
+        w: '1',
+        s: '2',
+        ava: '2',
+    },
+    mainGunName: 'Spitfire',
+    mainGunAttribute: {
+      dam: '14',
+      b:'4',
+      ammo: 'N',
+      save: 'ARM',
+      range: {
+        short_range:'0-8"',
+        short_mod: '0',
+        medium_range:'8-24"',
+        medium_mod: '+3',
+        long_range: '24-32"',
+        long_mod: '-3',
+        max_range: '32-48"',
+        max_attr: '-6'
+      },
+    },
+
+  },
+];
+
+//To make the selector (Something like tabs)
+const SELECTORS = [
+  { title: 'T&C', value: 0 },
+  { title: 'Privacy Policy', value: 1 },
+  { title: 'Return Policy', value: 2 },
+  { title: 'Reset all' },
+];
+
+export default class App extends Component {
+  state = {
+    //default active selector
+    activeSections: [],
+    //collapsed condition for the single collapsible
+    collapsed: true,
+    //multipleSelect is for the Multiple Expand allowed
+    //true: You can expand multiple at a time
+    //false: One can be expand at a time and other will be closed automatically
+    multipleSelect: false,
+  };
+
+  toggleExpanded = () => {
+    //Toggling the state of single Collapsible
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  setSections = sections => {
+    //setting up a active section state
+    this.setState({
+      activeSections: sections.includes(undefined) ? [] : sections,
+    });
+  };
+
+  renderHeader = (section, _, isActive) => {
+    //Accordion Header view
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor">
+        <Text style={styles.headerText}>{section.title}</Text>
+      </Animatable.View>
+    );
+  };
+
+  renderContent(section, _, isActive) {
+    //Accordion Content view
+    console.log(section);
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor">
+        <Animatable.Text
+          style={{ textAlign: 'center' }}>
+          {section.attribute.mov}
+          {section.mainGunName}
+          {section.title}
+          {section.mainGunAttribute.dam}
+          <Cart data={section}/>
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  }
+
+  render() {
+    const { multipleSelect, activeSections } = this.state;
+    return (
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
+          <Text style={styles.title}>Collapsible/Accordion Example</Text>
+
+          {/*Code for Single Collapsible Start*/}
+          <TouchableOpacity onPress={this.toggleExpanded}>
+            <View style={styles.header}>
+              <Text style={styles.headerText}>Single Collapsible</Text>
+              {/*Heading of Single Collapsible*/}
+            </View>
+          </TouchableOpacity>
+          {/*Content of Single Collapsible*/}
+          <Collapsible collapsed={this.state.collapsed} align="center">
+            <View style={styles.content}>
+              <Text style={{ textAlign: 'center' }}>
+                This is a dummy text of Single Collapsible View
+              </Text>
+            </View>
+          </Collapsible>
+          {/*Code for Single Collapsible Ends*/}
+
+          <View style={{ backgroundColor: '#000', height: 1, marginTop: 10 }} />
+          <View style={styles.multipleToggle}>
+            <Text style={styles.multipleToggle__title}>
+              Multiple Expand Allowed?
+            </Text>
+            <Switch
+              value={multipleSelect}
+              onValueChange={multipleSelect =>
+                this.setState({ multipleSelect })
+              }
+            />
           </View>
-          <View style={styles.underline}></View>
-          <View style={styles.rowContainer}>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>4-4</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>13</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>12</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>10</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>12</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>1</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>0</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>1</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>2</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>T</Text></View>
+          <Text style={styles.selectTitle}>
+            Please select below option to expand
+          </Text>
+
+          {/*Code for Selector starts here*/}
+          <View style={styles.selectors}>
+            {SELECTORS.map(selector => (
+              <TouchableOpacity
+                key={selector.title}
+                onPress={() => this.setSections([selector.value])}
+                //on Press of any selector sending the selector value to
+                // setSections function which will expand the Accordion accordingly
+              >
+                <View style={styles.selector}>
+                  <Text
+                    style={
+                      activeSections.includes(selector.value) &&
+                      styles.activeSelector
+                    }>
+                    {selector.title}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
           </View>
-        </View>
+          {/*Code for Selector ends here*/}
+         
+          {/*Code for Accordion/Expandable List starts here*/}
+          <Accordion
+            activeSections={activeSections}
+            //for any default active section
+            sections={ARMY_CONTENT}
+            //title and content of accordion
+            touchableComponent={TouchableOpacity}
+            //which type of touchable component you want
+            //It can be the following Touchables
+            //TouchableHighlight, TouchableNativeFeedback
+            //TouchableOpacity , TouchableWithoutFeedback
+            expandMultiple={multipleSelect}
+            //Do you want to expand mutiple at a time or single at a time
+            renderHeader={this.renderHeader}
+            //Header Component(View) to render
+            renderContent={this.renderContent}
+            //Content Component(View) to render
+            duration={400}
+            //Duration for Collapse and expand
+            onChange={this.setSections}
+            //setting the state of active sections
+          />
+          {/*Code for Accordion/Expandable List ends here*/}
+        </ScrollView>
       </View>
-      <View style={styles.cardContainer}>
-        <View style={styles.troopTitle}>
-          <Text style={styles.troopTitleText}>Multi Sniper (Anti-materiel Mode)</Text>
-        </View>
-        <View style={styles.detailContainer}>
-          <View style={styles.rowContainer}>
-            <View style={styles.rowWidth}><Text>Dam</Text></View>
-            <View style={styles.rowWidth}><Text>B</Text></View>
-            <View style={styles.rowWidth}><Text>Ammo</Text></View>
-            <View style={styles.rowWidth}><Text>Save</Text></View>
-          </View>
-          <View style={styles.underline}></View>
-          <View style={styles.rowContainer}>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>PH</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>1</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>N</Text></View>
-            <View style={styles.rowWidth}><Text style={styles.rowBodyText}>Arm</Text></View>
-          </View>
-          <View style={styles.rowContainer}>
-            <View style={styles.rowWidth}><Text>Short</Text></View>
-            <View style={styles.rowWidth}><Text>Medium</Text></View>
-            <View style={styles.rowWidth}><Text>Long</Text></View>
-            <View style={styles.rowWidth}><Text>Max</Text></View>
-          </View>
-          <View style={styles.underline}></View>
-          <View style={styles.rowContainer}>
-            <View style={[styles.rowWidth, { backgroundColor: Color.rangeYellow }]}><Text style={styles.rowBodyText}>0-8" -3</Text></View>
-            <View style={[styles.rowWidth, { backgroundColor: Color.rangeGreen }]}><Text style={styles.rowBodyText}>8-24" +3</Text></View>
-            <View style={[styles.rowWidth, { backgroundColor: Color.rangeYellow }]}><Text style={styles.rowBodyText}>24-40" -3</Text></View>
-            <View style={[styles.rowWidth, { backgroundColor: Color.rangeRed }]}><Text style={styles.rowBodyText}>40-48" -6</Text></View>
-          </View>
-        </View>
-      </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  detailContainer: {
-    paddingHorizontal: '3%',
-    width: '100%',
-  },
-  cardContainer: {
-    width: '100%',
-    margin: 10,
-    padding: 10,
-    alignItems: "center"
-  },
-  underline: {
-    width: '100%',
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    alignItems: "center",
-
-  },
-  troopTitle: {
-    padding: 10,
-    backgroundColor: Color.mainGrey,
-    width: '100%',
-
-  },
-  troopTitleText: {
-    fontSize: 18,
-    color: 'white',
-  },
-  rowBodyText: {
-    fontWeight: "bold",
-
-  },
-  rowHeaderText: {
-
-  },
   container: {
     flex: 1,
+    backgroundColor: '#F5FCFF',
+    paddingTop: 30,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  content: {
+    padding: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: '100%'
   },
-  listContainer: {
-    width: '100%',
-    alignItems: "center",
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
   },
-  rowContainer: {
-    flex: 1,
-    alignSelf: 'stretch',
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectors: {
+    marginBottom: 10,
     flexDirection: 'row',
-    alignItems: "center",
-
+    justifyContent: 'center',
   },
-  rowWidth: {
-    flex: 1,
-    alignSelf: 'stretch',
-    alignItems: "center",
-  }
-
+  selector: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: 'bold',
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10,
+    textAlign: 'center',
+  },
+  multipleToggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+    alignItems: 'center',
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
+  },
 });
