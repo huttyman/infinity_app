4575bd3064611d0a727560241876938b9081bf1d
huttyman_456 <tossapong.p@gmail.com>
2020-09-12 04:20:28 +0700

    Updates

---
+++ precache-manifest.3d58a858fc9aaf5c79008adcd1672df8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b746e588c79ba772fdc",
+    "url": "/infinity_app/static/js/app.309172ba.chunk.js"
+  },
+  {
+    "revision": "910ec9977756ac925f1c",
+    "url": "/infinity_app/static/js/runtime~app.66033198.js"
+  },
+  {
+    "revision": "f43f76c0ca7a1e212111",
+    "url": "/infinity_app/static/js/2.4b2e2c24.chunk.js"
+  },
+  {
+    "revision": "efa14d0ea434b2a091bbbbb88072f667",
+    "url": "/infinity_app/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/infinity_app/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/infinity_app/expo-service-worker.js"
+  },
+  {
+    "revision": "fb3d6a2e6bb4f6a37718a3da3c4b4776",
+    "url": "/infinity_app/register-service-worker.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/infinity_app/static/js/2.4b2e2c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/infinity_app/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/infinity_app/favicon-32.png"
+  },
+  {
+    "revision": "b27287954a8393ef9c70073b2fb0b988",
+    "url": "/infinity_app/favicon.ico"
+  },
+  {
+    "revision": "3d890f57e54de6d90a98da61b0b2c9af",
+    "url": "/infinity_app/manifest.json"
+  }
+];
